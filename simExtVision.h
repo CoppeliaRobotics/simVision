c669d0a5f4694d34cc0bea24dac6d88d7e55c52b
Coppelia <mfreese@gmx.ch>
2023-04-11 16:57:10 +0200

    small refact + fvisibility=hidden

--- simExtVision.h
+++ simExtVision.h
@@ -1,12 +1,6 @@
 #pragma once
 
-#ifdef _WIN32
-    #define SIM_DLLEXPORT extern "C" __declspec(dllexport)
-#endif /* _WIN32 */
-#if defined (__linux) || defined (__APPLE__)
-    #define SIM_DLLEXPORT extern "C"
-#endif /* __linux || __APPLE__ */
-
+#include <simLib/simExp.h>
 
 // The 3 required entry points of the CoppeliaSim plugin:
 SIM_DLLEXPORT unsigned char simStart(void* reservedPointer,int reservedInt);
--- simExtVision.pro
+++ simExtVision.pro
@@ -17,6 +17,7 @@ INCLUDEPATH += "../include"
 *-g++* {
     QMAKE_CXXFLAGS += -O3
     QMAKE_CXXFLAGS += -Wall
+    QMAKE_CXXFLAGS += -fvisibility=hidden
     QMAKE_CXXFLAGS += -Wno-unused-parameter
     QMAKE_CXXFLAGS += -Wno-strict-aliasing
     QMAKE_CXXFLAGS += -Wno-empty-body
